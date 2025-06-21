07c806ebbe4b0b7ffca4743c7789484f63a0cb37
Falcon Felipe <92966302+halcondorado123@users.noreply.github.com>
2025-06-20 21:03:26 -0500

    Upload_DB_Configuration
    Base de datos SQL para gestión de usuarios con CRUD completo. 
    Incluye:
    * Tablas (USERS + GENDER)
    * SPs con validaciones
    * Datos demo pre-cargados

---
+++ 02_DML_USER_MANAGER.sql
@@ -0,0 +1,18 @@
+INSERT INTO [UMA].[GENDER] (GENDER) VALUES 
+('Male'),
+('Female'),
+('Non-binary'),
+('Prefer not to say'),
+('Other');
+
+INSERT INTO [UMA].[USERS] (NAME_USER, BIRTH_DATE, ID_GENDER) VALUES 
+('Jhonattan Halcón', '1995-03-15', 1), -- Male
+('Laura Smith', '1992-07-21', 2), -- Female
+('Alex Taylor', '2000-11-10', 3), -- Non-binary
+('Chris Jordan', '1988-01-03', 4), -- Prefer not to say
+('Sam Morgan', '1999-06-25', 5), -- Other
+('Emily Davis', '1994-12-08', 2), -- Female
+('Michael Brown', '1990-05-18', 1), -- Male
+('Jordan Lee', '1998-09-30', 3), -- Non-binary
+('Taylor Kim', '2001-04-14', 4), -- Prefer not to say
+('Morgan Reese','1997-08-02', 5); -- Other
---
+++ 03_SP_USER_MANAGER.sql
@@ -0,0 +1,203 @@
+---------------- STORED PROCEDURES  -- PersonME --------------------
+
+-- NOTA: Copie, pegue y ejecute este script en su gestor de bases de datos (preferiblemente SQL Server). 
+-- Asegúrese de ajustar la sintaxis según sea necesario, sin modificar los valores de las columnas.
+
+-------------- CREATION INFORMATION -------------------
+
+-- CREATED BY: JHONATTAN HALCON CASALLAS FELIPE
+-- PROJECT: USER_MANAGER
+-- CREATION-DATE: 20/06/2025
+-- DESCRPTION: Procedimientos almacenados responsables de gestionar el proceso CRUD para el control de usuarios básicos, brindando soporte a la ejecución del proyecto desarrollado en .NET 8.
+
+------------------------------------------------------
+
+CREATE PROCEDURE [UMA].[SP_GET_USERS]
+AS
+BEGIN
+	SELECT 
+		USU.ID_USER,
+		USU.NAME_USER,
+		USU.BIRTH_DATE,
+		USU.ID_GENDER,
+		GEN.GENDER
+	FROM [UMA].[USERS] USU
+	LEFT JOIN [UMA].[GENDER] GEN ON USU.ID_GENDER = GEN.ID_GENDER;
+END;
+GO
+
+
+CREATE PROCEDURE [UMA].[SP_GET_USER_BY_ID]
+	@ID_USER INT
+AS
+BEGIN
+SET NOCOUNT ON;
+    IF NOT EXISTS (SELECT 1 FROM [UMA].[USERS] WHERE ID_USER = @ID_USER)
+    BEGIN
+        RAISERROR('No user was found with the specified User Id.', 16, 1);
+        RETURN;
+    END
+
+	SELECT 
+		USU.ID_USER,
+		USU.NAME_USER,
+		USU.BIRTH_DATE,
+		USU.ID_GENDER,
+		GEN.GENDER
+	FROM [UMA].[USERS] USU
+	LEFT JOIN [UMA].[GENDER] GEN ON USU.ID_GENDER = GEN.ID_GENDER
+	WHERE USU.ID_USER = @ID_USER
+END;
+GO
+
+
+CREATE PROCEDURE [UMA].[SP_INSERT_USER]
+	@NameUser NVARCHAR(100),
+    @DateOfBirth DATE,
+    @IdGender INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        IF @NameUser IS NULL OR LTRIM(RTRIM(@NameUser)) = ''
+            OR @DateOfBirth IS NULL
+			OR @IdGender IS NULL
+        BEGIN
+            RAISERROR('All required fields must be provided and cannot be empty.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+		IF NOT EXISTS (SELECT 1 FROM [UMA].[GENDER] WHERE ID_GENDER = @IdGender)
+		BEGIN
+			RAISERROR('Invalid gender selected.', 16, 1);
+			ROLLBACK TRANSACTION;
+			RETURN;
+		END
+
+        IF @DateOfBirth NOT BETWEEN DATEADD(YEAR, -95, CAST(GETDATE() AS DATE)) AND DATEADD(YEAR, -2, CAST(GETDATE() AS DATE))
+        BEGIN
+            RAISERROR('User must be between 2 and 95 years old.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        INSERT INTO [UMA].[USERS] (
+            NAME_USER,
+            BIRTH_DATE,
+            ID_GENDER
+        )
+        VALUES (
+            @NameUser,
+            @DateOfBirth,
+            @IdGender
+        );
+
+
+        DECLARE @NewUserId INT = SCOPE_IDENTITY();
+
+        COMMIT TRANSACTION;
+
+        SELECT @NewUserId AS ID_USER;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
+GO
+
+
+CREATE PROCEDURE [UMA].[SP_UPDATE_USER]
+	@UserId INT,
+	@NameUser NVARCHAR(100),
+    @DateOfBirth DATE,
+    @IdGender INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        IF @NameUser IS NULL OR LTRIM(RTRIM(@NameUser)) = ''
+            OR @DateOfBirth IS NULL
+			OR @IdGender IS NULL
+        BEGIN
+            RAISERROR('All required fields must be provided and cannot be empty.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        IF EXISTS (SELECT 1 FROM [UMA].[USERS] WHERE ID_GENDER = @IdGender)
+        BEGIN
+            RAISERROR('Gender must be selected', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        IF @DateOfBirth NOT BETWEEN DATEADD(YEAR, -95, CAST(GETDATE() AS DATE)) AND DATEADD(YEAR, -2, CAST(GETDATE() AS DATE))
+        BEGIN
+            RAISERROR('User must be between 2 and 95 years old.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+		UPDATE [UMA].[USERS]
+		SET
+			NAME_USER = @NameUser,
+			BIRTH_DATE = @DateOfBirth,
+			ID_GENDER = @IdGender
+		WHERE ID_USER = @UserId;
+
+        COMMIT TRANSACTION;
+
+        SELECT @UserId AS ID_USER;
+
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
+GO
+
+
+
+CREATE PROCEDURE [UMA].[SP_DELETE_USER]
+    @UserId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRANSACTION;
+
+    BEGIN TRY
+
+        IF NOT EXISTS (SELECT 1 FROM [UMA].[USERS] WHERE ID_USER = @UserId)
+        BEGIN
+            RAISERROR('No user found with the specified StudentId.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        DELETE FROM [UMA].[USERS] WHERE ID_USER = @UserId;
+
+        IF @@ROWCOUNT = 0
+        BEGIN
+            RAISERROR('Failed to delete the user. No records were removed.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        COMMIT TRANSACTION;
+        
+        SELECT 1 AS Status;
+
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
+GO
